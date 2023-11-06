0877ea2687995ca11648fbf9cc8a8ec18ad596d0
Steeve <s.descarpentries@octo.com>
2023-11-06 14:39:58 +0100

    Ajout des premier etst sur le découpage d'une phrase en mot.

--- usecases/SentenceSpliter.go
+++ usecases/SentenceSpliter.go
@@ -1,12 +1,21 @@
 package usecases
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 const BAD_ARGUMENTS_ERROR string = "bad arguments"
 
-func split(sentence *string) ([]string, error) {
-	if sentence == nil {
+func split(sentence string) ([]string, error) {
+	trimmed := strings.TrimSpace(sentence)
+	if trimmed == "" {
 		return nil, errors.New(BAD_ARGUMENTS_ERROR)
 	}
-	return nil, nil
+
+	return strings.FieldsFunc(trimmed, delimiters), nil
+}
+
+func delimiters(r rune) bool {
+	return r == ' ' || r == ',' || r == ';'
 }
--- usecases/SentenceSpliter_test.go
+++ usecases/SentenceSpliter_test.go
@@ -6,10 +6,75 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Nil_Input_Sentence_Should_Return_Error_Bad_Argument(t *testing.T) {
-	var sentence *string = nil
+func Test_Empty_Input_Sentence_Should_Return_Error_Bad_Argument(t *testing.T) {
+	var sentence string = ""
 
 	_, err := split(sentence)
 
 	assert.EqualError(t, err, BAD_ARGUMENTS_ERROR)
+}
+
+func Test_Non_Empty_Input_Sentence_without_space_Should_Return_String_List_Of_length_1(t *testing.T) {
+	var sentence string = "Steeve"
+
+	words, err := split(sentence)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(words))
+	assert.Equal(t, "Steeve", words[0])
+}
+
+func Test_Sentence_with_2_words_and_space_Should_Return_String_List_Of_length_2(t *testing.T) {
+	var sentence string = "Steeve Dess"
+
+	words, err := split(sentence)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(words))
+	assert.Equal(t, "Steeve", words[0])
+	assert.Equal(t, "Dess", words[1])
+}
+
+func Test_Sentence_with_2_words_and_comas_Should_Return_String_List_Of_length_2(t *testing.T) {
+	var sentence string = "Steeve, Dess"
+
+	words, err := split(sentence)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(words))
+	assert.Equal(t, "Steeve", words[0])
+	assert.Equal(t, "Dess", words[1])
+}
+
+func Test_Sentence_with_2_words_and_semicolon_Should_Return_String_List_Of_length_2(t *testing.T) {
+	var sentence string = "Steeve; Dess"
+
+	words, err := split(sentence)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(words))
+	assert.Equal(t, "Steeve", words[0])
+	assert.Equal(t, "Dess", words[1])
+}
+
+func Test_Sentence_with_3_words_and_spaces_Should_Return_String_List_Of_length_3(t *testing.T) {
+	var sentence string = "Steeve Elfie Lise"
+
+	words, err := split(sentence)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 3, len(words))
+	assert.Equal(t, "Steeve", words[0])
+	assert.Equal(t, "Elfie", words[1])
+	assert.Equal(t, "Lise", words[2])
+}
+
+func Test_One_Word_Sentence_ending_with_space_and_Cariage_Return_Should_Return_String_List_Of_length_1(t *testing.T) {
+	var sentence string = "Steeve \r\n"
+
+	words, err := split(sentence)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(words))
+	assert.Equal(t, "Steeve", words[0])
 }
